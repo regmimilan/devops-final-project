9152e8636f4c18f08ed32e73567a4926263008d1
regmimilan <milanregmi348@gmail.com>
2024-08-08 20:59:52 -0400

    Changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
+// src/App.test.js
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeElement = screen.getByText(/Welcome to DevOps Final Project/i);
+  expect(welcomeElement).toBeInTheDocument();
+});
+
+test('renders list of contributors', () => {
+  render(<App />);
+  const contributorsElement = screen.getByText(/Contributors of This Project are:/i);
+  expect(contributorsElement).toBeInTheDocument();
+
+  // Example test for specific contributors (adjust as per your actual list)
+  const milanElement = screen.getByText(/Milan Regmi/i);
+  const sujalElement = screen.getByText(/Sujal Shrestha/i);
+  const arunElement = screen.getByText(/Arun Osti/i);
+  const srijeshElement = screen.getByText(/Srijesh Khanal/i);
+
+  expect(milanElement).toBeInTheDocument();
+  expect(sujalElement).toBeInTheDocument();
+  expect(arunElement).toBeInTheDocument();
+  expect(srijeshElement).toBeInTheDocument();
 });
